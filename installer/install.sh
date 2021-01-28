e27f8b38cc4e2d04a3bc5228d7a693288807c13c
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-01-28 15:57:26 +0800

    fix:修复在线安装无法输入当前 IP 地址的问题

--- installer/install.sh
+++ installer/install.sh
@@ -246,9 +246,13 @@ ln -s /usr/local/bin/rsctl /usr/bin/rsctl
 
 ##配置 rackshift IP
 if [ ! $upgrade ]; then
-  printTitle "配置 RackShift 服务 ip 地址:"
-  echo "请输入 RackShift 当前IP地址(与物理机 PXE 口属于同一个 VLAN )："
+  if [ ! "$serverIp" ]; then
+  printTitle "配置 RackShift 服务 IP 地址:"
+  echo "请输入 RackShift 当前 IP 地址(与物理机 PXE 口属于同一个 VLAN )："
   read serverIp
+  else
+  printTitle "RackShift 服务 IP 地址: $serverIp"
+  fi
   cp /opt/rackshift/rackhd/monorail/config.json.bak /opt/rackshift/rackhd/monorail/config.json
   sed -i "s/172.31.128.1/${serverIp}/g" /opt/rackshift/rackhd/monorail/config.json
   sed -i "s/172.31.128.1/${serverIp}/g" /opt/rackshift/conf/mysql/sql/rackshift.sql
