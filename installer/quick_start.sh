eda10071804131e72c033e0cb5dc9cc028ee5ce2
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-03-11 16:51:19 +0800

    fix:修复在线安装脚本重复下载安装包，下载失败时任错误执行下去的问题

--- installer/quick_start.sh
+++ installer/quick_start.sh
@@ -1,16 +1,24 @@
 #Install Latest Stable RackShift Release
 os=$(uname -a)
-# 支持MacOS
+
 if [[ $os =~ 'Darwin' ]]; then
   echo 暂时不支持 MacOS 安装
-  #VERSION=$(curl -s https://github.com/rackshift/rackshift/releases/latest |grep -Eo 'v[0-9]+.[0-9]+.[0-9]+')
 else
   VERSION=$(curl -s https://github.com/rackshift/rackshift/releases/latest/download 2>&1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]+.*(?=")')
 fi
 
-wget --no-check-certificate https://github.com/rackshift/rackshift/releases/latest/download/rackshiftV${VERSION}.tar.gz
-#curl -s https://api.github.com/repos/rackshift/rackshift/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
-tar zxvf rackshiftV${VERSION}.tar.gz
+if [[ $VERSION =~ "" ]]; then
+  echo Please check your network,github is unreachable!
+  exit
+fi
+
+if [ ! -f ./rackshiftV${VERSION}.tar.gz ]; then
+      wget --no-check-certificate https://github.com/rackshift/rackshift/releases/latest/download/rackshiftV${VERSION}.tar.gz
+fi
+
+if [ ! -f ./rackshiftV${VERSION}/installer ]; then
+      tar zxvf rackshiftV${VERSION}.tar.gz
+fi
 cd rackshiftV${VERSION}/installer
 
 /bin/bash install.sh
